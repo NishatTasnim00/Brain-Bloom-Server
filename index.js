f37c6753f8af3eb8b8d87f0411938f27e9dfc5ed
Nishat <tasnim.nt00@gmail.com>
2023-11-17 06:30:03 +0600

    dlt implemented

--- index.js
+++ index.js
@@ -208,7 +208,7 @@ async function run() {
   });
 
  
-  app.delete("/deleteCourse/:id", async (req, res) => {
+  app.delete("/deleteCourse/:id", verifyJWT, async (req, res) => {
     const id = req.params.id;
     const query = { _id: new ObjectId(id) };
     const result = await coursesCollection.deleteOne(query);
